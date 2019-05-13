001292ff101597c61a4c449c6ec7a29fc7ef44f4
Mcc <mcc3939@gmail.com>
2019-05-14 01:43:17 +0700

    add input date page

---
+++ app/src/main/java/com/example/income_and_expense/InputDataActivity.kt
@@ -0,0 +1,12 @@
+package com.example.income_and_expense
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InputDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_data)
+    }
+}
--- app/src/main/java/com/example/income_and_expense/MainActivity.kt
+++ app/src/main/java/com/example/income_and_expense/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.income_and_expense
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_input_data.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_input_data)
+
+        inputDate.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) {
+                toast("true")
+            } else {
+                toast("false")
+            }
+        }
+    }
+
+    private fun toast(s: String) {
+        Toast.makeText(this@MainActivity, s, Toast.LENGTH_SHORT).show()
     }
 }
